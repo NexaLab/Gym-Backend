264f92f5ca4bc5eeb908eabdd9d048b0ade2e819
anaskhan3141 <anas.raheem2003@gmail.com>
2023-05-05 23:56:47 +0300

    starting and end date in membership

--- src/services/ClientMembershipService.js
+++ src/services/ClientMembershipService.js
@@ -7,7 +7,7 @@ module.exports = {
 
     getAll: async (req, res, next) => {
 
-        const query = 'SELECT cl.id, cm.client_id,cm.membership_id, cl.first_name, cl.last_name,cl.phone_no,cl.location,cl.user_id,u.email,m.status , m.price FROM client_membership cm inner join clients cl on cm.client_id = cl.id inner join users u on cl.user_id = u.id inner join membership m on m.id = cm.membership_id'
+        const query = 'SELECT cl.id, cm.client_id,cm.membership_id, cl.first_name, cl.last_name,cl.phone_no,cl.location,cl.user_id,u.email,m.status,cm.start_date,cm.end_date , m.price FROM client_membership cm inner join clients cl on cm.client_id = cl.id inner join users u on cl.user_id = u.id inner join membership m on m.id = cm.membership_id'
 
         db.query(query, (error, result) => {
 
